f48424d095ebcf16e15f28b86218ac1dd88ba89f
Tudinh Duong <tudinh@abstractfactory.io>
2014-04-10 21:22:01 +0100

    AJAX Title Fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,22 +4,30 @@ $(document).ready(function() {
 	$("#loading").hide();
 });
 
-jQuery(document).ready(function($) {
 
-    var siteUrl = 'http://'+(document.location.hostname||document.location.host);
+//	HISTORICIZE AND AJAXIFY OUR SITE
+jQuery(document).ready(function() {
+	var siteUrl = 'http://'+(document.location.hostname||document.location.host);
 
-    $(document).delegate('a[href^="/"],a[href^="'+siteUrl+'"]', "click", function(e) {
-        e.preventDefault();
-        History.pushState({}, "", this.pathname);
-        $("#loading").show();
-    });
+	//	Catch all internally-focused links and push a new state.
+	//	Note: External links will not be affected by this behavior.
+	$(document).delegate('a[href^="/"],a[href^="'+siteUrl+'"]', "click", function(e) {
+		e.preventDefault();
+		History.pushState({}, "", this.pathname);
+		$("#loading").show();
+	});
 
-    History.Adapter.bind(window, 'statechange', function(){
-        var State = History.getState();
-        $.get(State.url, function(data){
-            document.title = $(data).find("title").text();
-            $('.content').html($(data).find('.content'));
-            _gaq.push(['_trackPageview', State.url]);
+	History.Adapter.bind(window, 'statechange', function(){
+		var State = History.getState();
+		$.get(State.url, function(data){	// Use AJAX to get the new content.
+			document.title = data.match(/<title>(.*?)<\/title>/)[1];
+			$('.content').html($(data).find('.content')); 	// Pull the post we want out of the .content class.
+															// If you change the class of the post container,
+															// you must change it here!!!
+			_gaq.push(['_trackPageview', State.url]);	// This updates Google Analytics with a visit to the new page.
+														// If you don't use Google Analytics, you can safety comment or
+														// remove that line.
+
             tableofContents(); //init toc function
 			overviewPage(); //init overview function
 			
@@ -28,9 +36,8 @@ jQuery(document).ready(function($) {
 			 $( "#loading" ).fadeOut( "slow", function() {	
 			});
 
-        });
-    });
-
+		});
+	});
 });
 
 
